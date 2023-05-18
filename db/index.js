0ad35f8caa236d9c79311054e639acb563656908
BernardoGuedesAbreu <bernardoabreu1991@gmail.com>
2023-05-18 17:23:49 +0100

    lastday

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  `mongodb+srv://bernardoabreu:${process.env.MONGODB_PASSWORD}@project2.933oy1u.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://bernardoabreu:${process.env.MONGODB_PASSWORD}@project2.933oy1u.mongodb.net/?retryWrites=true&w=majority` 
 
 mongoose
   .connect(MONGO_URI)
